eb322b18b83f8b8d837a4e3872922d105f7e2aab
paul mcloughlin <cloggy45@googemail.com>
2018-08-14 21:53:36 +0100

    Added .gitignore in the SAMPLE_MODULES and SAMPLE_TREE

--- packages/git-extractor/src/routes/github/push/utils/__tests__/delta.test.ts
+++ packages/git-extractor/src/routes/github/push/utils/__tests__/delta.test.ts
@@ -3,31 +3,85 @@ import { ITree } from "../../";
 import { INormalizedModules } from "../../../../../utils/sandbox/normalize";
 
 describe("commit", () => {
-  describe("delta", () => {
-    const SAMPLE_TREE = [
-      {
-        path: "src/App.css",
-        mode: "100644",
-        type: "blob",
-        sha: "15adfdc710ca89d2c427dcbb6716943e1029c73a",
-        size: 341,
-        url:
-          "https://api.github.com/repos/CompuIves/codesandbox-test-git-app/git/blobs/15adfdc710ca89d2c427dcbb6716943e1029c73a"
-      },
-      {
-        path: "src/App.js",
-        mode: "100644",
-        type: "blob",
-        sha: "d7d52a7f38a321668d4fa83409a7c47d1bfccd7c",
-        size: 496,
-        url:
-          "https://api.github.com/repos/CompuIves/codesandbox-test-git-app/git/blobs/d7d52a7f38a321668d4fa83409a7c47d1bfccd7c"
-      }
-    ];
+    describe("delta", () => {
+        const SAMPLE_TREE = [
+            {
+                path: ".gitignore",
+                mode: "100644",
+                type: "blob",
+                sha: "c019777bd3de1d5dd3f26b59c6b6a89e15ba1ac6",
+                size: 570,
+                url: "https://api.github.com/repos/CompuIves/codesandbox-test-git-app/git/blobs/c019777bd3de1d5dd3f26b59c6b6a89e15ba1ac6"
+            },
+            {
+                path: "src/App.css",
+                mode: "100644",
+                type: "blob",
+                sha: "15adfdc710ca89d2c427dcbb6716943e1029c73a",
+                size: 341,
+                url:
+                    "https://api.github.com/repos/CompuIves/codesandbox-test-git-app/git/blobs/15adfdc710ca89d2c427dcbb6716943e1029c73a"
+            },
+            {
+                path: "src/App.js",
+                mode: "100644",
+                type: "blob",
+                sha: "d7d52a7f38a321668d4fa83409a7c47d1bfccd7c",
+                size: 496,
+                url:
+                    "https://api.github.com/repos/CompuIves/codesandbox-test-git-app/git/blobs/d7d52a7f38a321668d4fa83409a7c47d1bfccd7c"
+            }
+        ];
 
-    const SAMPLE_MODULES: INormalizedModules = {
-      "src/App.css": {
-        content: `.App {
+        const SAMPLE_MODULES: INormalizedModules = {
+            ".gitignore": {
+                content: `# See https://help.github.com/ignore-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+# See https://help.github.com/ignore-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+`,
+                isBinary: false,
+            }
+            "src/App.css": {
+                content: `.App {
   text-align: center;
 }
 
@@ -52,10 +106,10 @@ describe("commit", () => {
   to { transform: rotate(360deg); }
 }
 `,
-        isBinary: false
-      },
-      "src/App.js": {
-        content: `import React, { Component } from 'react';
+                isBinary: false
+            },
+            "src/App.js": {
+                content: `import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -77,55 +131,55 @@ class App extends Component {
 
 export default App;
 `,
-        isBinary: false
-      }
-    };
+                isBinary: false
+            }
+        };
 
-    it("detects no change", () => {
-      expect(getDelta(SAMPLE_TREE, SAMPLE_MODULES)).toEqual({
-        added: [],
-        deleted: [],
-        modified: []
-      });
+        it("detects no change", () => {
+            expect(getDelta(SAMPLE_TREE, SAMPLE_MODULES)).toEqual({
+                added: [],
+                deleted: [],
+                modified: []
+            });
+        });
+
+        it("detects added files", () => {
+            const newModules = {
+                ...SAMPLE_MODULES,
+                "test.js": { content: "Hey", isBinary: false }
+            };
+
+            expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
+                added: ["test.js"],
+                deleted: [],
+                modified: []
+            });
+        });
+
+        it("detects modified files", () => {
+            const newModules = {
+                ...SAMPLE_MODULES,
+                "src/App.js": { content: "Hey", isBinary: false }
+            };
+
+            expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
+                added: [],
+                deleted: [],
+                modified: ["src/App.js"]
+            });
+        });
+
+        it("detects deleted files", () => {
+            const newModules = {
+                ...SAMPLE_MODULES,
+                "src/App.js": null
+            };
+
+            expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
+                added: [],
+                deleted: ["src/App.js"],
+                modified: []
+            });
+        });
     });
-
-    it("detects added files", () => {
-      const newModules = {
-        ...SAMPLE_MODULES,
-        "test.js": { content: "Hey", isBinary: false }
-      };
-
-      expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
-        added: ["test.js"],
-        deleted: [],
-        modified: []
-      });
-    });
-
-    it("detects modified files", () => {
-      const newModules = {
-        ...SAMPLE_MODULES,
-        "src/App.js": { content: "Hey", isBinary: false }
-      };
-
-      expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
-        added: [],
-        deleted: [],
-        modified: ["src/App.js"]
-      });
-    });
-
-    it("detects deleted files", () => {
-      const newModules = {
-        ...SAMPLE_MODULES,
-        "src/App.js": null
-      };
-
-      expect(getDelta(SAMPLE_TREE, newModules)).toEqual({
-        added: [],
-        deleted: ["src/App.js"],
-        modified: []
-      });
-    });
-  });
 });
